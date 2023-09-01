f045199cc32fdd54ecc55184cff10fe57647d394
Juandazuluaga <131983905+Juandazuluaga@users.noreply.github.com>
2023-09-01 09:33:33 -0500

    Creado el  rpo

--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,18 +1,22 @@
 import { useState } from "react";
 import "./MiOrg.css";
 
-const MiOrg = () => {
+const MiOrg = (props) => {
   //Estado  - hooks
   // useState
-  //useState()
-  const manejarClick = () => {
-    console.log("Mostrar/Ocultar elemento");
-  };
+  //const [nombreVariable, funcionActualiza] = useSyate(valorInicial)
+  console.log(props);
+  // const [mostrar, actualizarMostrar] = useState(true);
+
+  // const manejarClick = () => {
+  //   console.log("Mostrar/Ocultar elemento", !mostrar);
+  //   actualizarMostrar(!mostrar);
+  // };
 
   return (
     <section className="orgSection">
-      <h3 className="title">Mi Organización</h3>
-      <img src="/img/add.png" alt="add" onClick={manejarClick} />
+      <h3 className="title">Mi Organización </h3>
+      <img src="/img/add.png" alt="add" onClick={props.cambiarMostrar} />
     </section>
   );
 };
